a5a963c8f99e43927668cc25027fb6285251336b
root <root@DESKTOP-07C9913.localdomain>
2019-01-14 00:53:52 +0800

    w

--- elasticc/elasticc.cpp
+++ elasticc/elasticc.cpp
@@ -1,11 +1,45 @@
 ﻿#include "elasticc.h"
 
+static void TestIndex() {
+	std::string res;
+	elasticc::Index(res, "localhost:9200", "twitter", "_doc", "{ \"user\":\"kimchy\",\"post_date\":\"2019-11-15T14:12:12\",\"message\":\"try elasticc\"}", 1);
+	std::cout << res << std::endl;
+}
+
+static void TestTryIndex() {
+	std::string res;
+	elasticc::TryIndex(res, "localhost:9200", "twitter", "_doc", "{ \"user\":\"kimchy\",\"post_date\":\"2019-11-15T14:12:12\",\"message\":\"try elasticc\"}", 1);
+	std::cout << res << std::endl;
+}
+
+static void TestGet() {
+	std::string res;
+	elasticc::Get(res, "localhost:9200", "twitter", "_doc", 1);
+	std::cout << res << std::endl;
+}
+
+static void TestDelete() {
+	std::string res;
+	elasticc::Delete(res, "localhost:9200", "twitter", "_doc", 1);
+	std::cout << res << std::endl;
+}
+
+static void TestUpdate() {
+	std::string res0;
+	elasticc::Index(res0, "localhost:9200", "test", "_doc", "{ \"counter\" : 1, \"tags\" : [\"red\"] }", 1);
+	std::cout << res0 << std::endl;
+	std::string res1;
+	elasticc::Update(res1, "localhost:9200", "test", "_doc","{ \"script\" : { \"source\": \"ctx._source.counter += params.count\", \"lang\": \"painless\", \"params\" : { \"count\" : 4 } } }", 1);
+	std::cout << res1 << std::endl;
+}
+
 int main()
 {
-	std::string res;
-	elasticc::Index(res,"","","",1);
-	std::cout << res << std::endl;
-
+	//TestIndex();
+	//TestTryIndex();
+	//TestGet();
+	//TestDelete();
+	//TestUpdate();
 	std::cin.get();
 	return 0;
 }
--- elasticc/elasticc.h
+++ elasticc/elasticc.h
@@ -4,11 +4,17 @@
 #include <string>
 #include "curl/curl.h"
 
-// Header-only Compatible Elastic Search C++ API (C++0x Compatible)
+// Header-only Elastic Search C++ API (C++0x Compatible)
 
 /*
 curl examples
-	curl -X PUT "localhost:9200/twitter/_doc/1" -H 'Content-Type: application/json' -d '{"user":"kimchy","post_date":"2009-11-15T14:12:12","message":"try eslaticc"}'
+
+curl -X PUT "localhost:9200/twitter/_doc/1" -H 'Content-Type: application/json' -d '{"user":"kimchy","post_date":"2009-11-15T14:12:12","message":"try eslaticc"}'
+
+CurlPutJson("localhost:9200/twitter/_doc/1",
+	"{ \"user\":\"kimchy\",\"post_date\":\"2019-11-15T14:12:12\",\"message\":\"try elasticc\"}",
+	result);
+	
 */
 
 namespace elasticc {
@@ -21,7 +27,14 @@ namespace elasticc {
 		writerData->append(data, sizes);
 		return sizes;
 	}
-	static inline bool CurlPutJson(const std::string& url, const std::string& json, std::string& result, bool verbose = false) {
+	enum : int {
+		HTTP_PUT = 1,
+		HTTP_GET = 2,
+		HTTP_POST = 3,
+		HTTP_DELETE = 4
+	};
+	static inline bool Curl(int httpmode, const std::string& url, std::string& result, bool verbose = false) 
+	{
 		CURL*    conn = NULL;
 		CURLcode code = CURLE_FAILED_INIT;
 		char     errorBuffer[CURL_ERROR_SIZE];
@@ -33,9 +46,85 @@ namespace elasticc {
 		// 设置url
 		code = curl_easy_setopt(conn, CURLOPT_URL, url.c_str());
 		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置put
-		code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "PUT");
+		// 设置put/post/get/delete
+		switch (httpmode) {
+		case HTTP_PUT:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "PUT"); 
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_GET:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "GET");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_POST:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "POST");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_DELETE:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "DELETE");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		default:
+			return false;
+		}
+		// 设置useragent
+		code = curl_easy_setopt(conn, CURLOPT_USERAGENT, "libcrp/0.1");
 		if (code != CURLE_OK) goto CurlPostExit;
+		// 设置error buffer
+		code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, errorBuffer);
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 支持http 3xx重定向
+		code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1);
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 设置response回写函数
+		code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, CurlWriter);
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 设置返回response的字符串容器
+		code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &result);
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 开始执行curl post
+		code = curl_easy_perform(conn);
+		if (code != CURLE_OK) goto CurlPostExit;
+	CurlPostExit:
+		if (conn) {
+			curl_easy_cleanup(conn);
+		}
+		return (code == CURLE_OK);
+	}
+	static inline bool CurlJson(int httpmode,const std::string& url, const std::string& json, std::string& result, bool verbose = false) 
+	{
+		CURL*    conn = NULL;
+		CURLcode code = CURLE_FAILED_INIT;
+		char     errorBuffer[CURL_ERROR_SIZE];
+		conn = curl_easy_init();
+		if (conn == NULL) goto CurlPostExit;
+		// 设置verbose模式
+		code = curl_easy_setopt(conn, CURLOPT_VERBOSE, verbose ? 1 : 0);
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 设置url
+		code = curl_easy_setopt(conn, CURLOPT_URL, url.c_str());
+		if (code != CURLE_OK) goto CurlPostExit;
+		// 设置put/post/get/delete
+		switch (httpmode) {
+		case HTTP_PUT:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "PUT"); 
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_GET:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "GET");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_POST:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "POST");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		case HTTP_DELETE:
+			code = curl_easy_setopt(conn, CURLOPT_CUSTOMREQUEST, "DELETE");
+			if (code != CURLE_OK) goto CurlPostExit;
+			break;
+		default:
+			return false;
+		}
 		// 设置json header
 		struct curl_slist *headers = NULL;
 		headers = curl_slist_append(headers, "Accept: application/json");
@@ -70,81 +159,59 @@ namespace elasticc {
 		}
 		return (code == CURLE_OK);
 	}
-	static inline bool CurlPost(const std::string& url, const std::string& postFields, std::string& result, bool verbose=false) {
-		CURL*    conn = NULL;
-		CURLcode code = CURLE_FAILED_INIT;
-		char     errorBuffer[CURL_ERROR_SIZE];
-		conn = curl_easy_init();
-		if (conn == NULL) goto CurlPostExit;
-		// 设置verbose模式
-		code = curl_easy_setopt(conn, CURLOPT_VERBOSE, verbose?1:0);
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置url
-		code = curl_easy_setopt(conn, CURLOPT_URL, url.c_str());
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置post
-		code = curl_easy_setopt(conn, CURLOPT_POST, 1);
-		if (code != CURLE_OK) goto CurlPostExit;
-		code = curl_easy_setopt(conn, CURLOPT_POSTFIELDS, postFields.c_str());
-		if (code != CURLE_OK) goto CurlPostExit;
-		code = curl_easy_setopt(conn, CURLOPT_POSTFIELDSIZE, postFields.size());
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置error buffer
-		code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, errorBuffer);
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 支持http 3xx重定向
-		code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1);
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置response回写函数
-		code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, CurlWriter);
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 设置返回response的字符串容器
-		code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &result);
-		if (code != CURLE_OK) goto CurlPostExit;
-		// 开始执行curl post
-		code = curl_easy_perform(conn);
-		if (code != CURLE_OK) goto CurlPostExit;
-	CurlPostExit:
-		if (conn) {
-			curl_easy_cleanup(conn);
-		}
-		return (code == CURLE_OK);
-	}
 
 	// [1] Index API
 	// 将文档放入index/type/id处，使之可搜索。若目标索引不存在，则创建该索引。id若为-1（默认值），则采用自动生成的id。
-	static inline void Index(std::string& result, const std::string& index, const std::string& type, const std::string& documentJson, int id = -1)
+	// 参数host是Elastic Search的host:ip字符串，必须包含端口号，例如"localhost:9200"。
+	// 参数documentJson字符串中的json变量必须用双引号，例如："{ \"user\":\"kimchy\",\"post_date\":\"2019-11-15T14:12:12\",\"message\":\"try elasticc\"}"。
+	static inline void Index(std::string& result, const std::string& host, const std::string& index, const std::string& type, const std::string& documentJson, int id = -1)
 	{
-		CurlPutJson("localhost:9200/twitter/_doc/1",
-					"{ \"user\":\"kimchy\",\"post_date\":\"2019-11-15T14:12:12\",\"message\":\"try elasticc\"}",
-					result);
+		std::string url= host + "/" + index + "/" + type + "/";
+		if (id != -1) url += std::to_string(id);
+		if (!CurlJson(HTTP_PUT, url, documentJson, result)) std::cerr << "Index failed\n";
 	}
 	// 功能同Index，只不过不会覆盖指定位置已存在的文档，而是返回操作失败信息。
-	static inline void TryIndex(std::string& result, const std::string& index, const std::string& type, const std::string& documentJson, int id=-1)
+	static inline void TryIndex(std::string& result, const std::string& host, const std::string& index, const std::string& type, const std::string& documentJson, int id=-1)
 	{
-
+		std::string url = host + "/" + index + "/" + type+"/";
+		if (id != -1) url += std::to_string(id);
+		url += "?op_type=create";
+		if(!CurlJson(HTTP_PUT, url, documentJson, result)) std::cerr << "TryIndex failed\n";
 	}
 
 	// [2] Get API
 	// 获取index/type/id处的文档。
-	static inline void Get(std::string& result, const std::string& index, const std::string& type, int id)
+	// 例如 curl -X GET "localhost:9200/twitter/_doc/0"
+	static inline void Get(std::string& result, const std::string& host, const std::string& index, const std::string& type, int id)
 	{
-
+		std::string url = host + "/" + index + "/" + type + "/" + std::to_string(id);
+		if(!Curl(HTTP_GET, url, result)) std::cerr << "Get failed\n";
 	}
 	// 获取index/type/id处的文档的field值。
-	static inline void GetField(std::string& result, const std::string& index, const std::string& type, int id, const std::string& field)
+	// 例如 curl -X GET "localhost:9200/twitter/_doc/1/_source"
+	static inline void GetField(std::string& result, const std::string& host, const std::string& index, const std::string& type, int id, const std::string& field)
 	{
-
+		std::string url = host + "/" + index + "/" + type + "/" + std::to_string(id) +"/"+ field;
+		if(!Curl(HTTP_GET, url, result)) std::cerr << "GetField failed\n";
 	}
 
-
 	// [3] Delete API
-	static inline void Delete(std::string& result, const std::string& index, const std::string& type, int id)
+	// 删除index/type/id处的文档。
+	// 例如 curl -X DELETE "localhost:9200/twitter/_doc/1"
+	static inline void Delete(std::string& result, const std::string& host, const std::string& index, const std::string& type, int id)
 	{
-
+		std::string url = host + "/" + index + "/" + type + "/" + std::to_string(id);
+		if(!Curl(HTTP_DELETE, url, result)) std::cerr << "Delete failed\n";
 	}
 
 	// [4] Update API
+	// POST一个更新脚本，对目标文档进行修改。
+	// 例如 curl -X POST "localhost:9200/test/_doc/1/_update" -H 'Content-Type: application/json' -d' { "script" : "ctx._source.new_field = \u0027value_of_new_field\u0027" } '
+	static inline void Update(std::string& result, const std::string& host, const std::string& index, const std::string& type, const std::string& updateScriptJson, int id)
+	{
+		std::string url = host + "/" + index + "/" + type + "/" + std::to_string(id)+"/_update";
+		if(!CurlJson(HTTP_POST, url, updateScriptJson, result)) std::cerr << "Update failed\n";
+	}
 
 
 
